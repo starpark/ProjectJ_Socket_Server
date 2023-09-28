1e8c280ffd26cf59b66c19346f060c01e144e09e
starpark <bbuckbyul@gmail.com>
2023-09-28 13:51:26 +0900

    feat protobuf messages

--- protobuf/GenCode.bat
+++ protobuf/GenCode.bat
@@ -1,14 +1,14 @@
 pushd %~dp0
 protoc.exe -I=./ --cpp_out=./ ./Enum.proto
 protoc.exe -I=./ --cpp_out=./ ./Struct.proto
-protoc.exe -I=./ --cpp_out=./ ./Packet.proto
+protoc.exe -I=./ --cpp_out=./ ./Message.proto
 
 XCOPY /Y Enum.pb.h "./Source"
 XCOPY /Y Enum.pb.cc "./Source"
 XCOPY /Y Struct.pb.h "./Source"
 XCOPY /Y Struct.pb.cc "./Source"
-XCOPY /Y Packet.pb.h "./Source"
-XCOPY /Y Packet.pb.cc "./Source"
+XCOPY /Y Message.pb.h "./Source"
+XCOPY /Y Message.pb.cc "./Source"
 
 DEL /Q /F *.pb.h
 DEL /Q /F *pb.cc
--- protobuf/Source/Enum.pb.h
+++ protobuf/Source/Enum.pb.h
@@ -45,22 +45,18 @@ struct TableStruct_Enum_2eproto {
 extern const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_Enum_2eproto;
 PROTOBUF_NAMESPACE_OPEN
 PROTOBUF_NAMESPACE_CLOSE
-namespace protocol {
+namespace ProjectJ {
 
 enum PacketType : int {
   PT_NONE = 0,
-  PT_C_JOIN = 1,
-  PT_S_JOIN = 2,
-  PT_C_LEAVE = 3,
-  PT_S_LEAVE = 4,
-  PT_C_MOVE = 5,
-  PT_S_MOVE = 6,
+  PT_C_VERIFY_TOKEN = 1,
+  PT_S_VERIFY_TOKEN = 2,
   PacketType_INT_MIN_SENTINEL_DO_NOT_USE_ = std::numeric_limits<int32_t>::min(),
   PacketType_INT_MAX_SENTINEL_DO_NOT_USE_ = std::numeric_limits<int32_t>::max()
 };
 bool PacketType_IsValid(int value);
 constexpr PacketType PacketType_MIN = PT_NONE;
-constexpr PacketType PacketType_MAX = PT_S_MOVE;
+constexpr PacketType PacketType_MAX = PT_S_VERIFY_TOKEN;
 constexpr int PacketType_ARRAYSIZE = PacketType_MAX + 1;
 
 const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* PacketType_descriptor();
@@ -76,31 +72,6 @@ inline bool PacketType_Parse(
     ::PROTOBUF_NAMESPACE_ID::ConstStringParam name, PacketType* value) {
   return ::PROTOBUF_NAMESPACE_ID::internal::ParseNamedEnum<PacketType>(
     PacketType_descriptor(), name, value);
-}
-enum ErrorCode : int {
-  EC_NONE = 0,
-  EC_S_SIGNUP_DUPLICATE_ID = 1,
-  ErrorCode_INT_MIN_SENTINEL_DO_NOT_USE_ = std::numeric_limits<int32_t>::min(),
-  ErrorCode_INT_MAX_SENTINEL_DO_NOT_USE_ = std::numeric_limits<int32_t>::max()
-};
-bool ErrorCode_IsValid(int value);
-constexpr ErrorCode ErrorCode_MIN = EC_NONE;
-constexpr ErrorCode ErrorCode_MAX = EC_S_SIGNUP_DUPLICATE_ID;
-constexpr int ErrorCode_ARRAYSIZE = ErrorCode_MAX + 1;
-
-const ::PROTOBUF_NAMESPACE_ID::EnumDescriptor* ErrorCode_descriptor();
-template<typename T>
-inline const std::string& ErrorCode_Name(T enum_t_value) {
-  static_assert(::std::is_same<T, ErrorCode>::value ||
-    ::std::is_integral<T>::value,
-    "Incorrect type passed to function ErrorCode_Name.");
-  return ::PROTOBUF_NAMESPACE_ID::internal::NameOfEnum(
-    ErrorCode_descriptor(), enum_t_value);
-}
-inline bool ErrorCode_Parse(
-    ::PROTOBUF_NAMESPACE_ID::ConstStringParam name, ErrorCode* value) {
-  return ::PROTOBUF_NAMESPACE_ID::internal::ParseNamedEnum<ErrorCode>(
-    ErrorCode_descriptor(), name, value);
 }
 // ===================================================================
 
@@ -120,19 +91,14 @@ inline bool ErrorCode_Parse(
 
 // @@protoc_insertion_point(namespace_scope)
 
-}  // namespace protocol
+}  // namespace ProjectJ
 
 PROTOBUF_NAMESPACE_OPEN
 
-template <> struct is_proto_enum< ::protocol::PacketType> : ::std::true_type {};
+template <> struct is_proto_enum< ::ProjectJ::PacketType> : ::std::true_type {};
 template <>
-inline const EnumDescriptor* GetEnumDescriptor< ::protocol::PacketType>() {
-  return ::protocol::PacketType_descriptor();
-}
-template <> struct is_proto_enum< ::protocol::ErrorCode> : ::std::true_type {};
-template <>
-inline const EnumDescriptor* GetEnumDescriptor< ::protocol::ErrorCode>() {
-  return ::protocol::ErrorCode_descriptor();
+inline const EnumDescriptor* GetEnumDescriptor< ::ProjectJ::PacketType>() {
+  return ::ProjectJ::PacketType_descriptor();
 }
 
 PROTOBUF_NAMESPACE_CLOSE
