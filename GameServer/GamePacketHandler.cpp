e399c2cb84e52b0792030f2e1842254219e919f0
starpark <bbuckbyul@gmail.com>
2023-09-28 13:54:03 +0900

    style make inline

--- GameServer/GamePacketHandler.cpp
+++ GameServer/GamePacketHandler.cpp
@@ -8,110 +8,18 @@
  *	Handler
  */
 
-bool Handle_Invalid(shared_ptr<GameSession> session, BYTE* bufer, int numOfBytes)
+bool Handle_Invalid(shared_ptr<GameSession>& session, BYTE* bufer, int numOfBytes)
 {
 	// TODO Log
 	return false;
 }
 
-bool Handle_C_JOIN(shared_ptr<GameSession> session, protocol::C_JOIN& packet)
+bool Handle_C_VERIFY_TOKEN(shared_ptr<GameSession>& session, ProjectJ::C_VERIFY_TOKEN& packet)
 {
-	static atomic<int> id = 0;
-
-	GLogHelper->WriteStdOut(LogCategory::Log_TEMP, L"Handle_C_JOIN\n");
-
-	return true;
-}
-
-bool Handle_C_LEAVE(shared_ptr<GameSession> session, protocol::C_LEAVE& packet)
-{
-	GLogHelper->WriteStdOut(LogCategory::Log_TEMP, L"count %d\n", packet.id());
-	return true;
-}
-
-bool Handle_C_MOVE(shared_ptr<GameSession> session, protocol::C_MOVE& packet)
-{
-	auto ownerService = session->GetOwnerService();
-
-	protocol::S_MOVE pkt;
-	pkt.set_id(packet.id());
-	protocol::Vector vector = pkt.vector();
-	vector = packet.vector();
-
-
-	//ownerService->BroadCastWithoutSelf(session, )
-
-	return true;
-}
-
-
-/*
- *	GamePacketHandler
- */
-
-void GamePacketHandler::Init()
-{
-	for (int i = 0; i < HANDLER_SIZE; i++)
-	{
-		handlers_[i] = Handle_Invalid;
-	}
-	BindHandler<protocol::C_JOIN>(protocol::PT_C_JOIN, Handle_C_JOIN);
-	BindHandler<protocol::C_LEAVE>(protocol::PT_C_LEAVE, Handle_C_LEAVE);
-	BindHandler<protocol::C_MOVE>(protocol::PT_C_MOVE, Handle_C_MOVE);
-}
-
-bool GamePacketHandler::HandlePacket(shared_ptr<GameSession> session, BYTE* buffer, int numOfBytes)
-{
-	auto header = reinterpret_cast<PacketHeader*>(buffer);
-	return handlers_[header->type](session, buffer, numOfBytes);
-}
-
-shared_ptr<SendBuffer> GamePacketHandler::MakeSendBuffer(protocol::S_JOIN& packet)
-{
-	return MakeSendBuffer(packet, protocol::PT_S_JOIN);
-}
-
-shared_ptr<SendBuffer> GamePacketHandler::MakeSendBuffer(protocol::S_LEAVE& packet)
-{
-	return MakeSendBuffer(packet, protocol::PT_S_LEAVE);
-}
-
-template <typename PacketMessage, typename HandlerFunc>
-void GamePacketHandler::BindHandler(protocol::PacketType type, HandlerFunc handler)
-{
-	handlers_[type] = [=](shared_ptr<class GameSession> session, BYTE* buffer, int numOfBytes)
-	{
-		return Handler<PacketMessage>(handler, session, buffer, numOfBytes);
-	};
-}
-
-template <typename PacketMessage, typename ProcessFunc>
-bool GamePacketHandler::Handler(ProcessFunc func, shared_ptr<GameSession> session, BYTE* buffer, int numOfBytes)
-{
-	PacketMessage packet;
-	if (packet.ParseFromArray(buffer + PacketHeaderSize, numOfBytes - PacketHeaderSize) == false)
-	{
-		return false;
-	}
-
-	return func(session, packet);
-}
-
-template <typename PacketMessage>
-shared_ptr<SendBuffer> GamePacketHandler::MakeSendBuffer(PacketMessage& packet, protocol::PacketType type)
-{
-	const uint16_t dataSize = static_cast<uint16_t>(packet.ByteSizeLong());
-	const uint16_t packetSize = dataSize + sizeof(PacketHeader);
-
-	auto sendBuffer = GSendBufferManager->GetSendBuffer(packetSize);
-	auto header = reinterpret_cast<PacketHeader*>(sendBuffer->GetBuffer());
-	header->size = packetSize;
-	header->type = type;
-
-	ASSERT_CRASH(packet.SerializeToArray(&header[1], dataSize));
-	sendBuffer->Close(packetSize);
-
-	return sendBuffer;
+	wstring token = wstring().assign(packet.token().begin(), packet.token().end());
+	GLogHelper->WriteStdOut(LogCategory::Log_TEMP, L"Handle_C_VERIFY_TOKEN: %d %s\n", packet.account_id(),
+	                        token.c_str());
+	return false;
 }
 
 
--- GameServer/GamePacketHandler.h
+++ GameServer/GamePacketHandler.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Packet.pb.h"
+#include "Message.pb.h"
 
 struct PacketHeader
 {
@@ -13,15 +13,13 @@ const int PacketHeaderSize = sizeof(PacketHeader);
  *	Handler
  */
 
-bool Handle_Invalid(shared_ptr<class GameSession> session, BYTE* bufer, int numOfBytes);
-bool Handle_C_JOIN(shared_ptr<class GameSession> session, protocol::C_JOIN& packet);
-bool Handle_C_LEAVE(shared_ptr<class GameSession> session, protocol::C_LEAVE& packet);
-bool Handle_C_MOVE(shared_ptr<class GameSession> session, protocol::C_MOVE& packet);
+bool Handle_Invalid(shared_ptr<class GameSession>& session, BYTE* bufer, int numOfBytes);
+bool Handle_C_VERIFY_TOKEN(shared_ptr<GameSession>& session, ProjectJ::C_VERIFY_TOKEN& packet);
 
 /*
  *	GamePacketHandler
  */
-using PacketHandler = function<bool(shared_ptr<class GameSession>, BYTE*, int)>;
+using PacketHandler = function<bool(shared_ptr<GameSession>&, BYTE*, int)>;
 
 class GamePacketHandler
 {
@@ -31,22 +29,65 @@ class GamePacketHandler
 	};
 
 public:
-	static void Init();
+	static void Init()
+	{
+		for (int i = 0; i < HANDLER_SIZE; i++)
+		{
+			handlers_[i] = Handle_Invalid;
+		}
+		BindHandler<ProjectJ::C_VERIFY_TOKEN>(ProjectJ::PT_C_VERIFY_TOKEN, Handle_C_VERIFY_TOKEN);
+	}
 
-	static bool HandlePacket(shared_ptr<class GameSession> session, BYTE* buffer, int numOfBytes);
+	static bool HandlePacket(shared_ptr<GameSession>& session, BYTE* buffer, int numOfBytes)
+	{
+		auto header = reinterpret_cast<PacketHeader*>(buffer);
+		return handlers_[header->type](session, buffer, numOfBytes);
+	}
 
-	static shared_ptr<SendBuffer> MakeSendBuffer(protocol::S_JOIN& packet);
-	static shared_ptr<SendBuffer> MakeSendBuffer(protocol::S_LEAVE& packet);
+
+	static shared_ptr<SendBuffer> MakeSendBuffer(ProjectJ::S_VERIFY_TOKEN& packet)
+	{
+		return MakeSendBuffer(packet, ProjectJ::PT_S_VERIFY_TOKEN);
+	}
 
 private:
 	template <typename PacketMessage, typename HandlerFunc>
-	static void BindHandler(protocol::PacketType type, HandlerFunc handler);
+	static void BindHandler(uint16_t type, HandlerFunc handler)
+	{
+		handlers_[type] = [=](shared_ptr<class GameSession> session, BYTE* buffer, int numOfBytes)
+		{
+			return Handler<PacketMessage>(handler, session, buffer, numOfBytes);
+		};
+	}
 
 	template <typename PacketMessage, typename ProcessFunc>
-	static bool Handler(ProcessFunc func, shared_ptr<class GameSession> session, BYTE* buffer, int numOfBytes);
+	static bool Handler(ProcessFunc func, shared_ptr<GameSession>& session, BYTE* buffer, int numOfBytes)
+	{
+		PacketMessage packet;
+		if (packet.ParseFromArray(buffer + PacketHeaderSize, numOfBytes - PacketHeaderSize) == false)
+		{
+			return false;
+		}
+
+		return func(session, packet);
+	}
 
 	template <typename PacketMessage>
-	static shared_ptr<SendBuffer> MakeSendBuffer(PacketMessage& packet, protocol::PacketType type);
+	static shared_ptr<SendBuffer> MakeSendBuffer(PacketMessage& packet, uint16_t type)
+	{
+		const uint16_t dataSize = static_cast<uint16_t>(packet.ByteSizeLong());
+		const uint16_t packetSize = dataSize + sizeof(PacketHeader);
+
+		auto sendBuffer = GSendBufferManager->GetSendBuffer(packetSize);
+		auto header = reinterpret_cast<PacketHeader*>(sendBuffer->GetBuffer());
+		header->size = packetSize;
+		header->type = type;
+
+		ASSERT_CRASH(packet.SerializeToArray(&header[1], dataSize));
+		sendBuffer->Close(packetSize);
+
+		return sendBuffer;
+	}
 
 private:
 	static PacketHandler handlers_[HANDLER_SIZE];
