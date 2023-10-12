06d4c12b599f985a9c016e02b10865052541bc35
starpark <bbuckbyul@gmail.com>
2023-10-12 21:29:22 +0900

    feat add libs

--- GameServer/pch.h
+++ GameServer/pch.h
@@ -3,9 +3,13 @@
 #ifdef _DEBUG
 #pragma comment(lib, "Debug\\ServerLibrary.lib")
 #pragma comment(lib, "Debug\\libprotobufd.lib")
+#pragma comment(lib, "Debug\\libcrypto.lib")
+#pragma comment(lib, "Debug\\libssl.lib")
 #else
 #pragma comment(lib, "Release\\ServerLibrary.lib")
 #pragma comment(lib, "Release\\libprotobuf.lib")
+#pragma comment(lib, "Release\\libcrypto.lib")
+#pragma comment(lib, "Release\\libssl.lib")
 #endif
 
 #include "ServerLibrary.h"
