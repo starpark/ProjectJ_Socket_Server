b3f7ddd258ce5f1da0b780178836cfd1cc8a9254
starpark <bbuckbyul@gmail.com>
2023-11-01 00:15:01 +0900

    fix thread local variable type

--- ServerLibrary/ThreadLocal.cpp
+++ ServerLibrary/ThreadLocal.cpp
@@ -3,5 +3,5 @@
 
 thread_local shared_ptr<SendBufferBlock> LSendBufferBlock = nullptr;
 thread_local std::stack<int> LLockStack;
-thread_local uint32_t LThreadId = 0;
-thread_local TickCounts LTickCounts;
+thread_local UINT32 LThreadID = 0;
+thread_local UINT64 LLastTick = 0;
--- ServerLibrary/ThreadLocal.h
+++ ServerLibrary/ThreadLocal.h
@@ -5,5 +5,5 @@
 
 extern thread_local shared_ptr<SendBufferBlock> LSendBufferBlock;
 extern thread_local std::stack<int> LLockStack;
-extern thread_local uint32_t LThreadId;
-extern thread_local TickCounts LTickCounts;
+extern thread_local UINT32 LThreadID;
+extern thread_local UINT64 LLastTick;
