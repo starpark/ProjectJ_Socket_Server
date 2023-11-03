cb25394885d8933265f0e74db321224c900476c7
starpark <bbuckbyul@gmail.com>
2023-11-03 21:32:53 +0900

    feat write log file

--- ServerLibrary/LogHelper.cpp
+++ ServerLibrary/LogHelper.cpp
@@ -1,10 +1,124 @@
 #include "pch.h"
 #include "LogHelper.h"
+#include <ctime>
+#include <string>
 
 LogHelper::LogHelper()
 {
+	namespace fs = std::filesystem;
+
 	stdOut_ = GetStdHandle(STD_OUTPUT_HANDLE);
-	stdErr_ = GetStdHandle(STD_ERROR_HANDLE);
+	UINT64 startTick = GetTickCount64();
+
+	fs::path path("Log");
+	path /= "LogFile_" + std::to_string(startTick) + ".txt";
+	create_directory(path.parent_path());
+
+	logFile_.open(path);
+}
+
+LogHelper::~LogHelper()
+{
+	logFile_.close();
+}
+
+void LogHelper::Reserve(LogCategory category, const WCHAR* format, ...)
+{
+	WRITE_LOCK;
+
+	LogWrapper log;
+	va_list list;
+
+	va_start(list, format);
+	log.category = category;
+
+	WCHAR buffer[BUFFER_SIZE];
+	vswprintf_s(buffer, BUFFER_SIZE, format, list);
+
+	wstring wideString(buffer);
+
+	log.buffer.assign(wideString.begin(), wideString.end());
+	va_end(list);
+
+	logQueue_.push(log);
+}
+
+void LogHelper::Reserve(LogCategory category, const char* format, ...)
+{
+	WRITE_LOCK;
+
+	LogWrapper log;
+	va_list list;
+
+	va_start(list, format);
+
+	log.category = category;
+
+	char buffer[BUFFER_SIZE];
+	vsprintf_s(buffer, BUFFER_SIZE, format, list);
+
+	log.buffer = string(buffer);
+
+	va_end(list);
+
+	logQueue_.push(log);
+}
+
+void LogHelper::Write()
+{
+	if (logQueue_.empty() == false)
+	{
+		LogWrapper log;
+
+		log = logQueue_.front();
+		logQueue_.pop();
+
+		SetColor(log.category);
+
+		string logString = "";
+
+		time_t now = time(NULL);
+		tm tm;
+		localtime_s(&tm, &now);
+
+		char date[64];
+		strftime(date, 64, "[%Y-%m-%d %H:%M:%S]", &tm);
+
+		logString += date;
+
+		switch (log.category)
+		{
+		case LogCategory::Log_INFO:
+			logString += "[Info] ";
+			break;
+		case LogCategory::Log_TEMP:
+			logString += "[Temp] ";
+			break;
+		case LogCategory::Log_ERROR:
+			logString += "[Error] ";
+			break;
+		case LogCategory::Log_WARN:
+			logString += "[Warn] ";
+			break;
+		case LogCategory::Log_SUCCESS:
+			logString += "[Success] ";
+			break;
+		}
+
+		logString += log.buffer;
+
+#ifdef _DEBUG
+		fprintf_s(stdout, logString.c_str());
+		fflush(stdout);
+#endif
+
+		SetColor(LogCategory::Log_INFO);
+
+		if (logFile_.is_open())
+		{
+			logFile_ << logString << flush;
+		}
+	}
 }
 
 void LogHelper::WriteStdOut(LogCategory category, const WCHAR* format, ...)
@@ -12,16 +126,16 @@ void LogHelper::WriteStdOut(LogCategory category, const WCHAR* format, ...)
 	if (format == nullptr)
 		return;
 
-	SetColor(true, category);
+	SetColor(category);
 
 	va_list ap;
 	va_start(ap, format);
-	vwprintf(format, ap);
+	vwprintf_s(format, ap);
 	va_end(ap);
 
 	fflush(stdout);
 
-	SetColor(true, LogCategory::Log_INFO);
+	SetColor(LogCategory::Log_INFO);
 }
 
 void LogHelper::WriteStdOut(LogCategory category, const char* format, ...)
@@ -29,27 +143,28 @@ void LogHelper::WriteStdOut(LogCategory category, const char* format, ...)
 	if (format == nullptr)
 		return;
 
-	SetColor(true, category);
+	SetColor(category);
 
 	va_list ap;
 	va_start(ap, format);
-	vprintf(format, ap);
+	vprintf_s(format, ap);
 	va_end(ap);
 
 	fflush(stdout);
 
-	SetColor(true, LogCategory::Log_INFO);
+	SetColor(LogCategory::Log_INFO);
 }
 
-void LogHelper::SetColor(bool stdOut, LogCategory category)
+void LogHelper::SetColor(LogCategory category)
 {
 	static WORD SColors[]
 	{
 		FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
 		FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN,
 		FOREGROUND_RED | FOREGROUND_INTENSITY,
-		FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
+		FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY,
+		FOREGROUND_GREEN | FOREGROUND_INTENSITY
 	};
 
-	SetConsoleTextAttribute(stdOut ? stdOut_ : stdErr_, SColors[static_cast<int>(category)]);
+	SetConsoleTextAttribute(stdOut_, SColors[static_cast<int>(category)]);
 }
--- ServerLibrary/LogHelper.h
+++ ServerLibrary/LogHelper.h
@@ -1,26 +1,48 @@
 #pragma once
 
-enum class LogCategory
+#include <fstream>
+#include <filesystem>
+
+enum class LogCategory : uint8_t
 {
 	Log_INFO,
 	Log_TEMP,
 	Log_ERROR,
-	Log_WARN
+	Log_WARN,
+	Log_SUCCESS
+};
+
+enum
+{
+	BUFFER_SIZE = 512
+};
+
+struct LogWrapper
+{
+	LogCategory category = LogCategory::Log_TEMP;
+	string buffer;
 };
 
 class LogHelper
 {
 public:
 	LogHelper();
+	~LogHelper();
 
 public:
+	void Reserve(LogCategory category, const WCHAR* format, ...);
+	void Reserve(LogCategory category, const char* format, ...);
 	void WriteStdOut(LogCategory category, const WCHAR* format, ...);
 	void WriteStdOut(LogCategory category, const char* format, ...);
+	void Write();
 
 protected:
-	void SetColor(bool stdOut, LogCategory category);
+	void SetColor(LogCategory category);
+
 
 private:
+	USE_LOCK;
 	HANDLE stdOut_;
-	HANDLE stdErr_;
+	ofstream logFile_;
+	queue<LogWrapper> logQueue_;
 };
