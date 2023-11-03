ab4a4442ef3bf2401bc129868dd23b180b5bee7a
starpark <bbuckbyul@gmail.com>
2023-11-03 21:30:47 +0900

    feat generate dummy client packet

--- Tools/PacketGenerator/PacketGenerator.py
+++ Tools/PacketGenerator/PacketGenerator.py
@@ -6,6 +6,7 @@ from datetime import datetime
 def main():
     arg_parser = argparse.ArgumentParser(description='PacketGenerator')
     arg_parser.add_argument('--path', type=str, default='./Message.proto', help='proto path')
+    arg_parser.add_argument('--output', type=str, default='TestPacketHandler', help='output file name')
     arg_parser.add_argument('--recv', type=str, default='C_', help='recv convention')
     arg_parser.add_argument('--send', type=str, default='S_', help='send convention')
     
@@ -15,49 +16,51 @@ def main():
 
     file_loader = jinja2.FileSystemLoader('Templates')
     env = jinja2.Environment(loader=file_loader)
-    data = datetime.today().strftime('%Y-%m-%d')
+    date = datetime.today().strftime('%Y-%m-%d')
 
     # GamePacketHandler for Server
-    template = env.get_template('GamePacketHandler.h')
-    output = template.render(parser=parser, date=data)
-    f = open('GamePacketHandler.h', 'w+', encoding='UTF8')
+    template = env.get_template('PacketHandler.h')
+    output = template.render(parser=parser, date=date, output=args.output)
+    f = open(args.output + '.h', 'w+', encoding='UTF8')
     f.write(output)
     f.close()
 
-    # JPacketHandler for Client
-    template = env.get_template('JPacketHandler.h')
-    output = template.render(parser=parser, date=data)
-    f = open('JPacketHandler.h', 'w+', encoding='UTF8')
-    f.write(output)
-    f.close()
+    if args.output == 'GamePacketHandler':
+        # JPacketHandler for Client
+        template = env.get_template('JPacketHandler.h')
+        output = template.render(parser=parser, date=date)
+        f = open('JPacketHandler.h', 'w+', encoding='UTF8')
+        f.write(output)
+        f.close()
 
-    template = env.get_template('JPacketHandler.cpp')
-    output = template.render(parser=parser, date=data)
-    f = open('JPacketHandler.cpp', 'w+', encoding='UTF8')
-    f.write(output)
-    f.close()
+        template = env.get_template('JPacketHandler.cpp')
+        output = template.render(parser=parser, date=date)
+        f = open('JPacketHandler.cpp', 'w+', encoding='UTF8')
+        f.write(output)
+        f.close()
 
-    # JPackets for Client
-    template = env.get_template('JPackets.h')
-    output = template.render(parser=parser, date=data)
-    f = open('JPackets.h', 'w+', encoding='UTF8')
-    f.write(output)
-    f.close()
+        # JPackets for Client
+        template = env.get_template('JPackets.h')
+        output = template.render(parser=parser, date=date)
+        f = open('JPackets.h', 'w+', encoding='UTF8')
+        f.write(output)
+        f.close()
 
-    with open('Message.pb.h', 'r+', encoding='UTF8') as file:
-        file_data = file.read()
-        file.seek(0, 0)
-        file.write('#pragma warning(disable:4800)\n'+ file_data)
+        # Add line disable warning 4800
+        with open('Message.pb.h', 'r+', encoding='UTF8') as file:
+            file_data = file.read()
+            file.seek(0, 0)
+            file.write('#pragma warning(disable:4800)\n'+ file_data)
 
-    with open('Struct.pb.h', 'r+', encoding='UTF8') as file:
-        file_data = file.read()
-        file.seek(0, 0)
-        file.write('#pragma warning(disable:4800)\n'+ file_data)
+        with open('Struct.pb.h', 'r+', encoding='UTF8') as file:
+            file_data = file.read()
+            file.seek(0, 0)
+            file.write('#pragma warning(disable:4800)\n'+ file_data)
 
-    with open('Enum.pb.h', 'r+', encoding='UTF8') as file:
-        file_data = file.read()
-        file.seek(0, 0)
-        file.write('#pragma warning(disable:4800)\n'+ file_data)
+        with open('Enum.pb.h', 'r+', encoding='UTF8') as file:
+            file_data = file.read()
+            file.seek(0, 0)
+            file.write('#pragma warning(disable:4800)\n'+ file_data)
 
 
     return
