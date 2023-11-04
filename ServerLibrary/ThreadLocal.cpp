b46504bba4f4a8ad9b0660b02512a6bc200b9e53
starpark <bbuckbyul@gmail.com>
2023-11-05 02:16:19 +0900

    feat thread time slice

--- ServerLibrary/ThreadLocal.cpp
+++ ServerLibrary/ThreadLocal.cpp
@@ -5,3 +5,4 @@ thread_local shared_ptr<SendBufferBlock> LSendBufferBlock = nullptr;
 thread_local std::stack<int> LLockStack;
 thread_local UINT32 LThreadID = 0;
 thread_local UINT64 LLastTick = 0;
+thread_local UINT64 LEndTickCount = 0;
--- ServerLibrary/ThreadLocal.h
+++ ServerLibrary/ThreadLocal.h
@@ -7,3 +7,4 @@ extern thread_local shared_ptr<SendBufferBlock> LSendBufferBlock;
 extern thread_local std::stack<int> LLockStack;
 extern thread_local UINT32 LThreadID;
 extern thread_local UINT64 LLastTick;
+extern thread_local UINT64 LEndTickCount;
