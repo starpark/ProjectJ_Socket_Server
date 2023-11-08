a390880db5549058d338e47c65a6720ecc1659bb
starpark <bbuckbyul@gmail.com>
2023-11-08 19:30:29 +0900

    feat thread safe timer

--- ServerLibrary/TimerTask.cpp
+++ ServerLibrary/TimerTask.cpp
@@ -2,34 +2,146 @@
 #include "TimerTask.h"
 
 
-void TimerTaskManager::Execute()
+TimerTaskManager::~TimerTaskManager()
 {
-	UINT64 nowTick = GetTickCount64();
-	if (nowTick >= LEndTickCount)
+}
+
+UINT32 TimerTaskManager::GetThreadId(UINT64 state)
+{
+	return static_cast<UINT32>(state >> 32);
+}
+
+
+/*
+void TimerTaskManager::ClearTimer(TimerHandle& handle)
+{
+	if (TimerData* timerData = FindTimer(handle))
+	{
+		switch (timerData->status)
+		{
+		case TimerStatus::Active:
+			timerData->status = TimerStatus::ActivePendingRemoval;
+			break;
+
+		case TimerStatus::Executing:
+			RemoveTimer(handle);
+			break;
+
+		case TimerStatus::ActivePendingRemoval:
+			// 이미 삭제가 진행된 상태
+			break;
+
+		default:
+			CRASH("Invalid Timer Status");
+		}
+	}
+	handle.Invalidate();
+}
+*/
+
+void TimerTaskManager::RemoveTimer(TimerHandle handle)
+{
+	if (TimerElementPtr element = handle.lock())
+	{
+		UINT64 prevState = element->state.fetch_or(RemovedState, std::memory_order_acquire);
+		int executingThreadId = GetThreadId(prevState);
+
+		while (executingThreadId != 0 && LThreadID != executingThreadId)
+		{
+			this_thread::yield();
+			executingThreadId = GetThreadId(element->state.load(std::memory_order_relaxed));
+		}
+	}
+}
+
+void TimerTaskManager::Distribute()
+{
+	WRITE_LOCK;
+
+	const UINT64 currentThreadId = static_cast<UINT64>(LThreadID) << 32;
+
+	auto PumpAddedElementsQueue = [this]()
+	{
+		while (TimerElementPtr addedElement = addedElements_.Pop())
+		{
+			UINT64 state = addedElement->state.load(std::memory_order_relaxed);
+			if (GetThreadId(state) == 0)
+			{
+				elements_.push_back(move(addedElement));
+			}
+		}
+	};
+
+	PumpAddedElementsQueue();
+
+	if (elements_.empty())
 	{
 		return;
 	}
 
-	vector<TimerItem> items;
+	currentTick = GetTickCount64();
+
+	vector<TimerElementPtr> timerElements;
+	int elementIdx = 0;
+
+	do
 	{
-		WRITE_LOCK;
-		while (timerTaskQueue_.empty() == false)
+		for (; elementIdx < elements_.size(); ++elementIdx)
 		{
-			const TimerItem& timerItem = timerTaskQueue_.top();
-			if (timerItem.executeTick_ > nowTick)
+			TimerElementPtr element = elements_[elementIdx];
+			UINT64 prevState = element->state.fetch_add(currentThreadId, std::memory_order_acquire);
+
+			ASSERT_CRASH(GetThreadId(prevState) == 0);
+
+			auto ClearExecutionFlag = [currentThreadId](TimerElementPtr& element)
 			{
-				break;
+				return static_cast<UINT8>(element->state.fetch_sub(currentThreadId, std::memory_order_release) -
+					currentThreadId);
+			};
+
+			if (prevState == RemovedState)
+			{
+				ClearExecutionFlag(element);
+				ObjectPool<TimerTask>::Push(element->taskData);
+
+				continue;
 			}
 
-			items.push_back(timerItem);
-			timerTaskQueue_.pop();
+			if (element->executeTick > currentTick)
+			{
+				ClearExecutionFlag(element);
+				timerElements.push_back(move(element));
+
+				continue;
+			}
+
+			if (auto object = element->taskData->object.lock())
+			{
+				object->Push(element->taskData->task, true);
+
+				prevState = ClearExecutionFlag(element);
+				if (prevState != RemovedState && element->isLoop)
+				{
+					ASSERT_CRASH(prevState == DefaultState);
+					element->executeTick = currentTick + element->delayTick;
+					timerElements.push_back(move(element));
+				}
+			}
+			else
+			{
+				prevState = ClearExecutionFlag(element);
+				ObjectPool<TimerTask>::Push(element->taskData);
+				ASSERT_CRASH(prevState == DefaultState || prevState == RemovedState);
+			}
 		}
+		PumpAddedElementsQueue();
 	}
+	while (elementIdx < elements_.size());
 
-	for (auto item : items)
+	elements_.clear();
 	{
-		auto taskPtr = item.task_;
-
-		taskPtr->Execute();
+		vector<TimerElementPtr> temp = move(timerElements);
+		timerElements = move(elements_);
+		elements_ = move(temp);
 	}
 }
--- ServerLibrary/TimerTask.h
+++ ServerLibrary/TimerTask.h
@@ -2,45 +2,80 @@
 
 #include "CommandTask.h"
 
-class TimerTask : public CommandTask
+using TickFuncType = function<void(float)>;
+
+struct TimerTask
 {
 public:
-	template <typename ObjectType, typename RetValType, typename... Args>
-	TimerTask(shared_ptr<ObjectType> object, RetValType (ObjectType::* memberFunc)(Args ...), Args&&... args)
-		: CommandTask(object, memberFunc, forward<Args>(args)...)
+	TimerTask(weak_ptr<CommandTaskObject> object, CommandTaskRef task)
+		: object(object), task(task)
 	{
 	}
+
+	weak_ptr<CommandTaskObject> object;
+	CommandTaskRef task;
 };
 
-struct TimerItem
+enum TimerStatus : UINT64
 {
-	bool operator<(const TimerItem& other) const
+	DefaultState = 0,
+	RemovedState = 1,
+};
+
+struct TimerData
+{
+	TimerData(bool inIsLoop, UINT64 inDelayTick, UINT64 inExecuteTick, TimerTask* inTaskData)
+		: isLoop(inIsLoop), delayTick(inDelayTick), executeTick(inExecuteTick),
+		  taskData(inTaskData)
 	{
-		return executeTick_ > other.executeTick_;
 	}
 
-	UINT64 executeTick_ = 0;
-	shared_ptr<TimerTask> task_ = nullptr;
+	bool operator<(const TimerData& other) const
+	{
+		return executeTick > other.executeTick;
+	}
+
+	bool isLoop = false;
+	UINT64 delayTick;
+	UINT64 executeTick;
+	TimerTask* taskData;
+	atomic<UINT64> state{DefaultState};
 };
+
+using TimerHandle = weak_ptr<TimerData>;
+using TimerElementPtr = shared_ptr<TimerData>;
 
 class TimerTaskManager
 {
 public:
-	template <typename ObjectType, typename RetValType, typename... Args>
-	void Reserve(UINT64 tickAfter, shared_ptr<ObjectType> object, RetValType (ObjectType::* memberFunc)(Args ...),
-	             Args&&... args)
-	{
-		const UINT64 executeTick = GetTickCount64() + tickAfter;
-		shared_ptr<TimerTask> task = ObjectPool<TimerTask>::MakeShared(object, memberFunc, forward<Args>(args)...);
+	~TimerTaskManager();
 
-		WRITE_LOCK;
-		timerTaskQueue_.push(TimerItem{executeTick, task});
+	template <typename ObjectType, typename RetValType, typename... Args>
+	TimerHandle AddTimer(UINT64 delayTick, bool isLoop, shared_ptr<ObjectType> object,
+	                     RetValType (ObjectType::* memberFunc)(Args ...),
+	                     Args&&... args)
+	{
+		static_assert(is_base_of_v<CommandTaskObject, ObjectType>, "The object must inherit from CommandTaskObject.");
+
+		shared_ptr<CommandTask> task = ObjectPool<CommandTask>::MakeShared(
+			static_pointer_cast<ObjectType>(object), memberFunc, forward<Args>(args)...);
+		TimerTask* timerTask = ObjectPool<TimerTask>::Pop(object, task);
+
+		UINT64 nowTick = GetTickCount64();
+		auto newTimerElement = make_shared<TimerData>(isLoop, delayTick, nowTick + delayTick, timerTask);
+		addedElements_.Push(newTimerElement);
+
+		return newTimerElement;
 	}
 
-	void Execute();
-
+	void RemoveTimer(TimerHandle handle);
+	void Distribute();
+	UINT32 GetThreadId(UINT64 state);
 
 private:
 	USE_LOCK;
-	priority_queue<TimerItem> timerTaskQueue_;
+	UINT64 currentTick = 0;
+	atomic<bool> distributing_ = false;
+	LockPriorityQueue<TimerElementPtr> addedElements_;
+	vector<TimerElementPtr> elements_;
 };
