f94331128eb8db8ecd90e00e2f9600f990c83927
starpark <bbuckbyul@gmail.com>
2023-11-14 00:11:42 +0900

    feat scale derived from inventory

--- GameServer/Scale.cpp
+++ GameServer/Scale.cpp
@@ -1,2 +1,16 @@
 #include "pch.h"
 #include "Scale.h"
+
+Scale::Scale(int index, int operatingWeight, int tolerance, int ivRow, int ivColumn)
+	: Inventory(ivRow, ivColumn, SCALE_MAX_WEIGHT), index_(index), operatingWeight_(operatingWeight), tolerance_(tolerance)
+{
+}
+
+Scale::~Scale()
+{
+}
+
+bool Scale::IsOperating()
+{
+	return operatingWeight_ - tolerance_ <= GetCurrentWeight() && GetCurrentWeight() <= operatingWeight_ + tolerance_;
+}
--- GameServer/Scale.h
+++ GameServer/Scale.h
@@ -1,6 +1,23 @@
 #pragma once
+#include "Inventory.h"
 
-class Scale
+class Scale : public Inventory
 {
-};
+	enum
+	{
+		SCALE_MAX_WEIGHT = 999'999'999
+	};
 
+public:
+	Scale(int index, int operatingWeight, int tolerance, int ivRow, int ivColumn);
+	~Scale() override;
+
+	bool IsOperating();
+	int GetOperatingWeight() const { return operatingWeight_; }
+	int GetTolerance() const { return tolerance_; }
+
+private:
+	int index_;
+	int operatingWeight_;
+	int tolerance_;
+};
