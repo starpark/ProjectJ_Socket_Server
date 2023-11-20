9436b712eb92f244b056209948c5e2518a105670
starpark <bbuckbyul@gmail.com>
2023-11-21 01:15:06 +0900

    feat mutex access player

--- GameServer/Player.cpp
+++ GameServer/Player.cpp
@@ -5,7 +5,7 @@
 
 
 Player::Player(int index, int row, int column, int maxWeight, int sessionID, string nickname)
-	: Inventory(index, row, column, maxWeight), sessionID_(sessionID), nickname_(nickname)
+	: Inventory(index, row, column, maxWeight), state_(ProjectJ::MatchPlayerState::NONE), score_(0), sessionID_(sessionID), nickname_(nickname)
 {
 }
 
@@ -14,14 +14,33 @@ Player::~Player()
 	GLogHelper->Print(LogCategory::LOG_INFO, L"~Player()\n");
 }
 
-
-void Player::SetInfo(ProjectJ::Vector position, ProjectJ::Rotator rotation)
+ProjectJ::PlayerInfo* Player::GetPlayerInfo()
 {
-	worldPosition_.x_ = position.x();
-	worldPosition_.y_ = position.y();
-	worldPosition_.z_ = position.z();
+	auto playerInfo = new ProjectJ::PlayerInfo();
+	auto position = new ProjectJ::Vector();
+	auto rotation = new ProjectJ::Rotator();
+	auto player = new ProjectJ::Player();
 
-	worldRotation_.pitch_ = rotation.pitch();
-	worldRotation_.roll_ = rotation.roll();
-	worldRotation_.yaw_ = rotation.yaw();
+	{
+		READ_LOCK;
+		position->set_x(worldPosition_.x_);
+		position->set_y(worldPosition_.y_);
+		position->set_z(worldPosition_.z_);
+
+		rotation->set_pitch(worldRotation_.pitch_);
+		rotation->set_roll(worldRotation_.roll_);
+		rotation->set_yaw(worldRotation_.yaw_);
+
+		playerInfo->set_state(state_);
+	}
+
+	player->set_account_id(GetID());
+	player->set_nickname(GetNickname());
+
+	playerInfo->set_player_index(index_);
+	playerInfo->set_allocated_player(player);
+	playerInfo->set_allocated_position(position);
+	playerInfo->set_allocated_rotation(rotation);
+
+	return playerInfo;
 }
--- GameServer/Player.h
+++ GameServer/Player.h
@@ -13,13 +13,85 @@ public:
 	shared_ptr<GameSession> GetOwnerSession() { return ownerSession_.lock(); }
 	int GetID() const { return sessionID_; }
 	string GetNickname() const { return nickname_; }
-	Vector GetVector() const { return worldPosition_; }
-	Rotator GetRotator() const { return worldRotation_; }
+	ProjectJ::MatchPlayerState GetState() const { return state_.load(); }
 
-	void SetSession(const shared_ptr<GameSession>& session) { ownerSession_ = session; }
-	void SetInfo(ProjectJ::Vector position, ProjectJ::Rotator rotation);
+	Vector GetVector()
+	{
+		READ_LOCK;
+		return worldPosition_;
+	}
+
+	Rotator GetRotation()
+	{
+		READ_LOCK;
+		return worldRotation_;
+	}
+
+	void SetSession(const shared_ptr<GameSession>& session)
+	{
+		WRITE_LOCK;
+		ownerSession_ = session;
+	}
+
+	void SetVector(const Vector& position)
+	{
+		WRITE_LOCK;
+		worldPosition_ = position;
+	}
+
+	void SetVector(Vector&& position)
+	{
+		WRITE_LOCK;
+		worldPosition_ = move(position);
+	}
+
+	void SetRotation(const Rotator& rotation)
+	{
+		WRITE_LOCK;
+		worldRotation_ = rotation;
+	}
+
+	void SetRotatoin(Rotator&& rotation)
+	{
+		WRITE_LOCK;
+		worldRotation_ = move(rotation);
+	}
+
+	void SetTransform(const Vector& position, const Rotator& rotation)
+	{
+		WRITE_LOCK;
+		worldPosition_ = position;
+		worldRotation_ = rotation;
+	}
+
+	void SetTransform(Vector&& position, Rotator&& rotation)
+	{
+		WRITE_LOCK;
+		worldPosition_ = move(position);
+		worldRotation_ = move(rotation);
+	}
+
+	void SetState(ProjectJ::MatchPlayerState state)
+	{
+		state_.store(state);
+	}
+
+	void AddScore(int score)
+	{
+		score_.fetch_add(score);
+	}
+
+	int GetScore() const
+	{
+		return score_.load();
+	}
+
+	ProjectJ::PlayerInfo* GetPlayerInfo();
 
 private:
+	USE_LOCK;
+	atomic<ProjectJ::MatchPlayerState> state_;
+	atomic<int> score_;
 	int sessionID_;
 	string nickname_;
 	Vector worldPosition_;
