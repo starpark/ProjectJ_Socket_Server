957c12b34b9c7391108572cbbf328f6936c5ecf8
starpark <bbuckbyul@gmail.com>
2023-11-21 01:06:44 +0900

    feat multiple lock macro

--- ServerLibrary/Macro.h
+++ ServerLibrary/Macro.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#define USE_MANY_LOCKS(count)	Lock _locks[count]
-#define USE_LOCK				USE_MANY_LOCKS(1)
-#define	READ_LOCK_IDX(idx)		ReadLockGuard readLockGuard_##idx(_locks[idx], typeid(this).name())
-#define READ_LOCK				READ_LOCK_IDX(0)
-#define	WRITE_LOCK_IDX(idx)		WriteLockGuard writeLockGuard_##idx(_locks[idx], typeid(this).name())
-#define WRITE_LOCK				WRITE_LOCK_IDX(0)
+#define USE_MANY_LOCKS(count)			Lock locks_[count]
+#define USE_LOCK						USE_MANY_LOCKS(1)
+#define	READ_LOCK_IDX(idx)				ReadLockGuard readLockGuard_##idx(locks_[idx], typeid(this).name())
+#define READ_LOCK						READ_LOCK_IDX(0)
+#define	WRITE_LOCK_IDX(idx)				WriteLockGuard writeLockGuard_##idx(locks_[idx], typeid(this).name())
+#define WRITE_LOCK						WRITE_LOCK_IDX(0)
+#define ADOPT_WRITE_LOCK				WriteLockGuard writeLockGuard_##idx(locks_[idx], typeid(this).name(), adopt_lock_t)
+#define USE_MULTIPLE_LOCK				Lock& GetLockRef() {return std::ref(locks_[0]);}
+#define GET_CLASS_NAME					typeid(this).name()
 
 #define CRASH(cause)						\
 {											\
